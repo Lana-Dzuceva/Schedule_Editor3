bfaa7c52cb74c2c02c7e79ce9127220517905905
Lana <lanadzuceva@gmail.com>
2022-10-29 18:49:30 +0300

    first init

---
+++ SheduleEditorV2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SheduleEditorV2
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tabControlGroups = new System.Windows.Forms.TabControl();
+            this.listViewErrors = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.хммToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 443;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tabControlGroups);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listViewErrors);
+            this.splitContainer2.Size = new System.Drawing.Size(353, 426);
+            this.splitContainer2.SplitterDistance = 290;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tabControlGroups
+            // 
+            this.tabControlGroups.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlGroups.Location = new System.Drawing.Point(0, 0);
+            this.tabControlGroups.Name = "tabControlGroups";
+            this.tabControlGroups.SelectedIndex = 0;
+            this.tabControlGroups.Size = new System.Drawing.Size(353, 290);
+            this.tabControlGroups.TabIndex = 0;
+            // 
+            // listViewErrors
+            // 
+            this.listViewErrors.BackColor = System.Drawing.Color.White;
+            this.listViewErrors.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewErrors.HideSelection = false;
+            this.listViewErrors.Location = new System.Drawing.Point(0, 0);
+            this.listViewErrors.Name = "listViewErrors";
+            this.listViewErrors.Size = new System.Drawing.Size(353, 132);
+            this.listViewErrors.TabIndex = 2;
+            this.listViewErrors.UseCompatibleStateImageBehavior = false;
+            this.listViewErrors.View = System.Windows.Forms.View.Details;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.хммToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // хммToolStripMenuItem
+            // 
+            this.хммToolStripMenuItem.Name = "хммToolStripMenuItem";
+            this.хммToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.хммToolStripMenuItem.Text = "Хмм";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(122, 20);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Редактор расписания";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TabControl tabControlGroups;
+        private System.Windows.Forms.ListView listViewErrors;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem хммToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ SheduleEditorV2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SheduleEditorV2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            listViewErrors.Columns.Add("Тип ошибки");
+            listViewErrors.Columns.Add("Сведения");
+            listViewErrors.Columns.Add("Еще сведелния");
+            listViewErrors.Columns.Add("Какая-то цифра");
+            for (int i = 0; i < 4; i++)
+            {
+                listViewErrors.Columns[i].Width = 140;
+            }
+            listViewErrors.Font = new Font(FontFamily.GenericSansSerif, 12);
+            for (int i = 0; i < 10; i++)
+            {
+                ListViewItem lvi = new ListViewItem("Error");
+                lvi.SubItems.Add("very");
+                lvi.SubItems.Add("bad");
+                lvi.SubItems.Add(i.ToString());
+                listViewErrors.Items.Add(lvi);
+                TabPage tabPage = new TabPage("hmm");
+                tabControlGroups.Controls.Add(tabPage);
+            }
+
+        }
+    }
+}
